3156f24dc72070b29b9b5d788050c3541d3e16f7
PKJrod <66578390+PKJrod@users.noreply.github.com>
2021-01-30 19:36:27 -0800

    Issue#4 create product db (#7)
    * Created Product model
    
    * Setup DbContext
    
    * AddedDbContext to startup. Added EF SQL Server NuGet Package
    
    * Added database connection string
    
    * Added EF Core Tools package
    
    * Added migration to create DB with products table

---
+++ eCommerceSite/Data/ProductContext.cs
@@ -0,0 +1,23 @@
+﻿using eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+// In order to install EF Core for web design Right click the name of project ie. eCommerceSite -> manage nuget packages -> install -> Microsoft.EntityFrameworkCore to be able to get
+// EF core to start working so we can start using database and connecting them with our website.
+
+namespace eCommerceSite.Data
+{   
+    public class ProductContext : DbContext // requires using Microsoft.EntityFrameworkCore
+    {
+        /// <summary>
+        /// A constructor that going to setup our connection string or whatever else we need from it
+        /// </summary>
+        /// <param name="products"></param>
+        public ProductContext(DbContextOptions<ProductContext> items)
+            : base(items){}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ eCommerceSite/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    /// <summary>
+    /// A sellable product
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// The products unique identification number, EF will automatically make it a primary key if it is a int and has a Id in the name.
+        /// can also force to make column as primary key using [Key] has to have using System.ComponentModel.DataAnnotations;
+        /// </summary>
+        [Key]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// The Consumer facing name of the product
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The retail price as US currency
+        /// </summary>
+        public double price { get; set; }
+
+        /// <summary>
+        /// Category product falls under. Ex. Electronics, Furniture, ..etc
+        /// </summary>
+        public string Category { get; set; }
+    }
+}
