607501ac9a3c7e196a90076e2948794d08e5331d
PKJrod <66578390+PKJrod@users.noreply.github.com>
2021-02-06 22:02:24 -0800

    Issue#5 add product (#17)
    * Adding Product is implemented
    
    * merged changes from styling into this branch
    
    * Style Product added Successfully
    
    * refactored DB code to run async

--- eCommerceSite/Controllers/ProductController.cs
+++ eCommerceSite/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerceSite.Data;
 using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,15 +21,16 @@ namespace eCommerceSite.Controllers
 
         /// <summary>
         /// Displays a view that lists all products
+        /// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/async
         /// </summary>
         /// <returns></returns> 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             //Get all products from database
             // List<Product> products = _context.Products.ToList();
             List<Product> products =
-                (from p in _context.Products
-                 select p).ToList();
+                await (from p in _context.Products
+                 select p).ToListAsync();
 
             // Send list of products to view to be displayed
             return View(products);
@@ -47,20 +49,22 @@ namespace eCommerceSite.Controllers
         /// <summary>
         /// Add the user input and then stores it to the database, in order to add razor right click method name "Add" ->
         /// Add View -> Razor View -> Create View.
+        /// Asynchronous code is much more efficient than synchronous code because everything is being runned at once, anytime we are
+        /// doing database code we will be using Asynchronous code.
         /// </summary>
         /// <param name="p"></param>
         /// <returns></returns>
         [HttpPost]
-        public IActionResult Add(Product p)
+        public async Task<IActionResult> Add(Product p)
         {
             if(ModelState.IsValid)
             {
                 // Add to DB
                 _context.Products.Add(p);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
 
                 // viewdata and tempdata are almost the exact same thing, viewdata will last on the current request, tempdata will last over one redirect
-                TempData["Message"] = $"{p.ProductId}:{p.Title} was added successfully";
+                TempData["Message"] = $"{p.Title} was added successfully";
                 
                 // redirect back to catalog page
                 return RedirectToAction("Index");
