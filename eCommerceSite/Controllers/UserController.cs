40551f7f66f3d2c1710c204c9c8411c26b29a617
Justin Rodriguez <justinrodriguez@live.com>
2021-02-16 20:50:02 -0800

    Added login error message for invalid credentials

--- eCommerceSite/Controllers/UserController.cs
+++ eCommerceSite/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerceSite.Data;
 using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -51,6 +52,49 @@ namespace eCommerceSite.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        /// <summary>
+        /// After info is submitted will do the check to make sure if user entered in correct credentials
+        /// SingleOrDefaultAsync - Single(will not have two users with the same email and password), default(will find the account and populate or set it to null)
+        /// </summary>
+        /// <param name="model">model is from UserAccount -> the class name LoginViewModel</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            UserAccount account =
+               await (from u in _context.UserAccounts
+                where (u.Username == model.UsernameOfEmail
+                    || u.Email == model.UsernameOfEmail)
+                    && u.Password == model.password
+                select u).SingleOrDefaultAsync();
+            /* * method syntax below query syntax above. sometimes can only use method syntax to access database
+             * UserAccount account = 
+             *      await _context.UserAccounts
+             *              .Where(userAcc => (userAcc.Username == model.UsernameOrEmail ||
+             *                              userAcc.Email == model.UsernameOrEmail) &&
+             *                              userAcc.Password == model.Password)
+             *              .SingleOrDefaultAsync();
+             * 
+             * */
+            if(account ==  null)
+            {
+                // Credentials did not match
+
+                // Custom error message
+                ModelState.AddModelError(string.Empty, "Credentials were not found");
+                return View(model);
+            }
+
+            // Log user, into website
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
