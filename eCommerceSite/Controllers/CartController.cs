ad8f5da125a43e559d7d945dbb2e13c45f7941f9
Justin Rodriguez <justinrodriguez@live.com>
2021-03-15 22:35:43 -0700

    Refactored cart controller

--- eCommerceSite/Controllers/CartController.cs
+++ eCommerceSite/Controllers/CartController.cs
@@ -32,35 +32,7 @@ namespace eCommerceSite.Controllers
             // Get product from the database
             Product p = await ProductDb.GetProductAsync(_context, id);
 
-            const string CartCookie = "CartCookie";
-
-            // Get existing cart items. https://www.c-sharpcorner.com/article/asp-net-core-working-with-cookie/
-            string existingItems = _httpContext.HttpContext.Request.Cookies[CartCookie];
-
-            List<Product> cartProducts = new List<Product>();
-
-            // looking to see if the cart has something attached to it
-            if(existingItems != null )
-            {   
-                // will deserialize the object and puts it in a list to have a cart working
-                cartProducts = JsonConvert.DeserializeObject<List<Product>>(existingItems);
-            }
-
-            // Add current product to existing cart
-            cartProducts.Add(p);
-            
-            // Add product list to cart cookie
-            string data = JsonConvert.SerializeObject(cartProducts);
-
-            CookieOptions options = new CookieOptions()
-            {
-                // cookie will last as long as you set it however users can always delete them.
-                Expires = DateTime.Now.AddYears(1),
-                Secure = true,
-                IsEssential = true
-            };
-
-            _httpContext.HttpContext.Response.Cookies.Append(CartCookie, data, options);
+            CookieHelper.AddProductToCart(_httpContext, p);
 
             // Redirect back to previous page
             return RedirectToAction("Index", "Product");
@@ -68,6 +40,7 @@ namespace eCommerceSite.Controllers
 
         public IActionResult Summary()
         {
+            /*
             // grabs the cookies data
             string cookieData = _httpContext.HttpContext.Request.Cookies["CartCookie"];
 
@@ -76,6 +49,11 @@ namespace eCommerceSite.Controllers
 
             // Display all products in shopping cart cookie
             return View(cartProducts);
+            // shorter method below
+            */
+
+            // Display all products in shopping cart cookie
+            return View(CookieHelper.GetCartProducts(_httpContext));
         }
     }
 }
